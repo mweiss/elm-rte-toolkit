8b2e67d848a23ca49f64857b2fd7ba94951ad7ed
mweiss <michael.josh.weiss@gmail.com>
2020-04-07 14:53:43 +0900

    add missing files

---
+++ precache-manifest.7bc6621eaa475a20e5eb8c7599b9b748.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "55a8d560871dcb181538f84b485b1a55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f4f80d518932cb9e238e",
+    "url": "/static/css/main.f15c13a8.chunk.css"
+  },
+  {
+    "revision": "f4f80d518932cb9e238e",
+    "url": "/static/js/main.f4f80d51.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "46ceac17e38e9a946f69",
+    "url": "/static/js/vendors~main.46ceac17.chunk.js"
+  }
+]);
