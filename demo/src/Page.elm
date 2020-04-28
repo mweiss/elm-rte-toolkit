d4ca217caa1eb0c2afd6c55d966281d10abbffcd
mweiss <michael.josh.weiss@gmail.com>
2020-04-28 22:52:08 +0900

    revert article grammarly attribute

--- demo/src/Page.elm
+++ demo/src/Page.elm
@@ -45,7 +45,7 @@ fontAwesomeStyle =
 
 viewContent : List (Html msg) -> Html msg
 viewContent content =
-    article [ Html.Attributes.attribute "data-gramm_editor" "false" ] content
+    article [] content
 
 
 viewHeader : Page -> Html msg
